f8e2cce7d5dc7e640a10a4aebff6d11e5e8a4607
lcmaas4 <lcmaas4@gmail.com>
2026-05-13 21:40:53 -0400

    align node versions and fix homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,19 @@
-import "./globals.css";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
+import './globals.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import { Metadata } from 'next';
 
-export const metadata = {
-  title: "Your App",
+export const metadata: Metadata = {
+  title: 'Phi Delt Northeastern',
+  description: 'The Sunset Project',
+  manifest: '/manifest.json',
+  icons: {
+    icon: [
+      { rel: 'icon', url: '/favicon-32x32.png', sizes: '32x32' },
+      { rel: 'icon', url: '/favicon-16x16.png', sizes: '16x16' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({
